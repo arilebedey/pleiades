8e300b76ce1bde3169e89cd68c590c675b723561
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-03-29 20:31:02 +0200

    init: greet page

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,10 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge } from 'electron';
+
+contextBridge.exposeInMainWorld('pleiades', {
+  platform: process.platform,
+  versions: {
+    chrome: process.versions.chrome,
+    electron: process.versions.electron,
+    node: process.versions.node,
+  },
+});
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,33 +1,35 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/latest/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './index.css';
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via webpack',
-);
+declare global {
+  interface Window {
+    pleiades: {
+      platform: string;
+      versions: {
+        chrome: string;
+        electron: string;
+        node: string;
+      };
+    };
+  }
+}
+
+const platform = document.querySelector('#platform');
+const electronVersion = document.querySelector('#electron-version');
+const chromeVersion = document.querySelector('#chrome-version');
+const nodeVersion = document.querySelector('#node-version');
+
+if (platform) {
+  platform.textContent = window.pleiades.platform;
+}
+
+if (electronVersion) {
+  electronVersion.textContent = window.pleiades.versions.electron;
+}
+
+if (chromeVersion) {
+  chromeVersion.textContent = window.pleiades.versions.chrome;
+}
+
+if (nodeVersion) {
+  nodeVersion.textContent = window.pleiades.versions.node;
+}
