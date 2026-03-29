4de991d43198aca3351f42c916026a5b06ef8e88
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-03-29 20:22:18 +0200

    init: webpack + ts template

--- src/main/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require("electron");
-
-contextBridge.exposeInMainWorld("pleiades", {
-  platform: process.platform,
-  versions: process.versions
-});
--- src/renderer/renderer.js
+++
@@ -1,9 +0,0 @@
-const platform = document.querySelector("#platform");
-const electronVersion = document.querySelector("#electron-version");
-const chromeVersion = document.querySelector("#chrome-version");
-const nodeVersion = document.querySelector("#node-version");
-
-platform.textContent = window.pleiades.platform;
-electronVersion.textContent = window.pleiades.versions.electron;
-chromeVersion.textContent = window.pleiades.versions.chrome;
-nodeVersion.textContent = window.pleiades.versions.node;
